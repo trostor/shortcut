7dd6a2e953d18ac0c76c92296bb89c17fd489b3e
astrostor <126304725+trostor@users.noreply.github.com>
2025-04-26 22:03:43 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+{\rtf1\ansi\ansicpg950\cocoartf2706
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 self.addEventListener('install', event => \{\
+  console.log('Service Worker \uc0\u23433 \u35037 \u23436 \u25104 ');\
+\});\
+\
+self.addEventListener('fetch', event => \{\
+  // \uc0\u31777 \u21934 \u29256 \u19981 \u20570 \u38626 \u32218 \u32233 \u23384 \
+\});\
+}
