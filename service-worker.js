9421f59b1eba68134d9e002ccd6ca19d876a971b
astrostor <126304725+trostor@users.noreply.github.com>
2025-04-26 22:33:34 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,10 @@
-{\rtf1\ansi\ansicpg950\cocoartf2706
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+// service-worker.js
 
-\f0\fs24 \cf0 self.addEventListener('install', event => \{\
-  console.log('Service Worker \uc0\u23433 \u35037 \u23436 \u25104 ');\
-\});\
-\
-self.addEventListener('fetch', event => \{\
-  // \uc0\u31777 \u21934 \u29256 \u19981 \u20570 \u38626 \u32218 \u32233 \u23384 \
-\});\
-}
+self.addEventListener('install', event => {
+  console.log('Service Worker 安裝完成');
+});
+
+self.addEventListener('fetch', event => {
+  // 這裡可以做離線緩存（進階功能）
+  // 目前我們先不做，保持簡單
+});
